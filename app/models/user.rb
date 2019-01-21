8cc9fbc617bfa38634611e2ec02c85de2cd0c1b4
georgejeffery <georgejeffery@gmail.com>
2019-01-21 17:22:56 +0000

    Apparently have to do this before pull?

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   has_secure_password
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :players
   resources :team_players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
