a8dd9c8217fb12c73c06de8ff4901d8825528935
github username <github email address>
2019-01-21 17:16:14 +0000

    Basic create account form done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true,
+                      length: { in: 5..20}
 end
