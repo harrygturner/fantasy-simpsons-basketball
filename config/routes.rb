d719a0878af437486f2fd9f0fb698bbad04b06e1
github username <github email address>
2019-01-22 09:24:21 +0000

    Authetication progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new', as: 'login_path'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :matches
   resources :users, only: %i[index new show create destroy]
   resources :teams
