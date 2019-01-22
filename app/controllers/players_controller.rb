a24531cda14d990d7683f88c3919e5415e19acd4
georgejeffery <georgejeffery@gmail.com>
2019-01-22 14:39:34 +0000

    Player Cards

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,12 @@
 class PlayersController < ApplicationController
-  before_action :require_login   
+  before_action :require_login
+  before_action :find_player, only: [:show]
+
+
+  def index
+    @players = Player.all
+  end
+  
   private
 
   def find_player
