e85f4905fd9931274d9e3e8f052155f78812b1e3
Dark-Raine <anderson-tomas@hotmail.com>
2019-01-22 14:31:59 +0000

    create team view

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,27 @@
 class TeamsController < ApplicationController
+  
+  def new
+    @team = Team.new
+    @players = Player.all
+    
+  end
+
+  def create
+    @team = Team.create(team_params)
+    byebug
+    redirect_to team_path(@team)
+  end
+  
+  def show
+    @team = Team.find(params[:id])
+  end
   private
 
   def find_team
     @team = Team.find(params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(:name, :user_id)
   end
 end
