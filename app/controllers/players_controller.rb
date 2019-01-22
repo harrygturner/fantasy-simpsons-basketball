8f76ed9b6b2acf1d1273d493b6d18b0b3720b6b3
georgejeffery <georgejeffery@gmail.com>
2019-01-22 16:41:51 +0000

    Random Change

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,7 +2,6 @@ class PlayersController < ApplicationController
   before_action :require_login
   before_action :find_player, only: [:show]
 
-
   def selectplayers
     @user = User.find(session[:user_id])
     @team = @user.team
@@ -11,25 +10,24 @@ class PlayersController < ApplicationController
     if @team.players.count == 0
       @players = Player.all
     else
-      @players = Player.all.reject{|player| @team.players.include?(player)}
+      @players = Player.all.reject { |player| @team.players.include?(player) }
       # byebug
     end
     @players
-
-    render 'index'
+    
   end
 
   def index
     @players = Player.all
   end
-  
-  
+
   def addplayer
     @user = User.find(session[:user_id])
     TeamPlayer.create(team_id: @user.team.id, player_id: params[:player_id])
     # byebug
     redirect_to players_path
   end
+
   private
 
   def find_player
